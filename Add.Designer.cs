b2d947db42db30586541bf9e9f9ba2eb7c8df82d
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2021-11-22 13:13:32 +0100

    isincn

--- Add.Designer.cs
+++ Add.Designer.cs
@@ -3,6 +3,7 @@
     partial class Add
     {
         /// <summary>
+        /// ojdnijdc
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
